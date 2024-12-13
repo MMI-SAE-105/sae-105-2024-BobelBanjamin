ff6ec84d2c28879f5b1ce1759e763362b4dd2e07
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2024-12-13 11:40:44 +0300

    caroussel html + css + java

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,25 @@ menuToggle.addEventListener('click', () => {
   }
 });
 
+//  CAROUSEL 
+const carousel = document.querySelector(".carouselcontainer");
+const prevButton = document.querySelector(".carouselbutton--prev");
+const nextButton = document.querySelector(".carouselbutton--next");
+const premierItem = document.querySelector(".carouselitem");
+
+
+// Largeur de défilement d’un item
+if (carousel) { 
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    const scrollAmount = premierItem.clientWidth;
+    carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+  });
+}
+// TODO
